4e1abe0326c4a83a2e23a8d766931fb11dd98e64
Ariji8 <arijit.mahanty@datatusk.com>
2023-07-24 19:31:20 +0000

    GoyaFinalupdatedDemo

--- tests/Login.spec.js
+++ tests/Login.spec.js
@@ -11,10 +11,10 @@ test.describe('test', () => {
 
     });
     
-    test('Failed Scenario', async () => {
+    test('Login Logout', async () => {
 
         await page.goto('https://portal-test.goya.com/oms2/#/home')
-        //Add assertions to check element
+        //Addd asssertions to chheck element
         await expect(page.getByPlaceholder('Username')).toHaveCount(1);
         await expect(page.getByPlaceholder('Password')).toHaveCount(1);
         await expect(page.getByRole('button', { name: 'Login' })).toBeEnabled();
@@ -22,10 +22,10 @@ test.describe('test', () => {
         await page.getByPlaceholder('Username').fill('013506');
         await page.getByPlaceholder('Password').fill('Pwd@013506');
         await page.getByRole('button', { name: 'Login' }).click();
-        //Add assertions to verify the URL of the home page .
+        //Addd asssertions to verify the URL of the home page .
         await expect(page).toHaveURL('https://portal-test.goya.com/oms2/#/home');
         await page.getByRole('button', { name: '013506-CARLOS MORATO' }).click();
-        await page.getByRole('link', { name: 'Logoutt' }).click();
+        await page.getByRole('link', { name: 'Logout' }).click();
         //Add assertion to verify that after logout we are getting back to the login screen.
         await expect(page).toHaveURL('https://portal-test.goya.com/oms2/#/login');
         //await page.close();
